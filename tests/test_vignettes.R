d8695574aa71e09f18f2ed8d48df461d756042f0
Paul R Staab <staab@bio.lmu.de>
2013-11-27 14:43:01 +0100

    remove check for vignettes

--- tests/test_vignettes.R
+++
@@ -1,5 +0,0 @@
-jaatha.howto <- system.file("doc/jaatha_howto.pdf", package="jaatha", mustWork=TRUE)
-csi.howto <- system.file("doc/custom_simulator_howto.pdf", package="jaatha", mustWork=TRUE)
-
-file.info(jaatha.howto)[1, "size"] > 75000 || stop("Seems we are shipping a dummy vignette")
-file.info(csi.howto)[1, "size"] > 75000 || stop("Seems we are shipping a dummy vignette")
