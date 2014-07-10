335b7683782285f1251aabf9d0b3277c7a00d2fe
Paul R Staab <staab@bio.lmu.de>
2014-07-10 11:12:34 +0200

    remove dependency to foreach package

--- R/Jaatha.R
+++ R/Jaatha.R
@@ -8,34 +8,6 @@
 # Licence:  GPLv3 or later
 # --------------------------------------------------------------
 
-
-#' Fast estimation of demographic parameters
-#' 
-#' Jaatha is a composite maximum likelihood method to estimate parameters
-#' of a speciation model of closely related biological species out of SNP
-#' data.
-#' 
-#' @author
-#' Lisha Naduvilezhath \email{lisha (at) biologie.uni-muenchen.de},
-#' Paul R. Staab \email{staab (at) biologie.uni-muenchen.de} and 
-#' Dirk Metzler \email{metzler (at) biologie.uni-muenchen.de}
-#'
-#' Maintainer: Paul R. Staab \email{staab (at) biologie.uni-muenchen.de}
-#' @name jaatha-package
-#' @docType package
-#' @title Fast estimation of demographic parameters
-#' @keywords package
-#' @importFrom phyclust ms
-#' @importFrom foreach foreach 
-#' @importFrom foreach %do% 
-#' @importFrom methods new 
-#' @importFrom methods representation 
-#' @importFrom parallel mclapply
-#' @importFrom Rcpp evalCpp
-#' @importFrom dplyr as.tbl_cube
-#' @useDynLib jaatha
-NULL
-
 #' The "Jaatha" S4 class saves the basic parameters for a Jaatha estimation
 #' procedure
 #'
