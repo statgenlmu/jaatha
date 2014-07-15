e3b4238e4b46505b0b6f870b9e9d824a72a8c53f
Paul R Staab <staab@bio.lmu.de>
2014-07-15 14:22:02 +0200

    improve self-tests for reproducibility

--- inst/unitTests/runit.sim-prog-ms.R
+++ inst/unitTests/runit.sim-prog-ms.R
@@ -9,26 +9,32 @@ test.callMS <- function() {
 }
 
 test.msSimFunc <- function() {
+  # Simple tt model with jsfs
   set.seed(789)
   sum.stats <- msSingleSimFunc(dm.tt, c(1,10))
   checkTrue(is.list(sum.stats))
   checkEquals(2, length(sum.stats))
-
   checkTrue(!is.null(sum.stats$pars))
   checkEquals(c(1,10), sum.stats$pars)
-
   checkTrue(!is.null(sum.stats$jsfs))
   checkTrue(is.array(sum.stats$jsfs))
   checkEquals(c(12,13), dim(sum.stats$jsfs))
   checkTrue(sum(sum.stats$jsfs) > 0)
-
-  set.seed(456)
+  
+  # Reproducibilty
+  set.seed(789)
+ 
+  sum.stats2 <- msSingleSimFunc(dm.tt, c(1,10))
+  checkEquals(sum.stats, sum.stats2)
+  
+  
+  # With segregating sites stastic
   dm@sum.stats <- data.frame()
   dm <- dm.addSummaryStatistic(dm, 'seg.sites')
+  set.seed(456)
   sum.stats <- msSingleSimFunc(dm, c(1, .5))
   checkEquals(2, length(sum.stats))
   checkTrue(!is.null(sum.stats$pars))
-
   checkTrue(!is.null(sum.stats$seg.sites))
   checkTrue( is.list(sum.stats$seg.sites))
   checkEquals(7, length(sum.stats$seg.sites))
@@ -36,6 +42,11 @@ test.msSimFunc <- function() {
     checkTrue( is.matrix(sum.stats$seg.sites[[i]]) )
     checkEquals(21, nrow(sum.stats$seg.sites[[i]]))
   }
+  
+  # Reproducibilty for producing seg.sites
+  set.seed(456)
+  sum.stats2 <- msSingleSimFunc(dm, c(1, .5))
+  checkEquals(sum.stats, sum.stats2)
 
   dm <- dm.addSummaryStatistic(dm, 'file')
   sum.stats <- msSingleSimFunc(dm, c(1, .5))
@@ -46,16 +57,26 @@ test.msSimFunc <- function() {
   checkTrue(file.exists(sum.stats$file))
   checkTrue((file.info(sum.stats$file)$size > 0))
 
+  # With FPC Statistic
   set.seed(123)
   dm@sum.stats <- data.frame()
   dm <- dm.addSummaryStatistic(dm, 'fpc')
   dm <- calcFpcBreaks(dm, sum.stats$seg.sites) 
+  
+  set.seed(13579)
   sum.stats <- msSingleSimFunc(dm, c(1, 10))
   checkEquals(2, length(sum.stats))
   checkTrue(!is.null(sum.stats$pars))
-  checkTrue(is.array(sum.stats[['fpc']]))
+  checkTrue(is.array(sum.stats[['fpc']]))  
   checkEquals(dm.getLociNumber(dm), sum(sum.stats[['fpc']]))
+  
+  # Reproducibitly FPC
+  set.seed(13579)
+  sum.stats2 <- msSingleSimFunc(dm, c(1, 10))
+  checkEquals(sum.stats, sum.stats2)
 
+  
+  # Multiple Statistics
   dm <- dm.addSummaryStatistic(dm, 'seg.sites')
   sum.stats <- msSingleSimFunc(dm, c(1, 0.1))
   checkEquals(3, length(sum.stats))
