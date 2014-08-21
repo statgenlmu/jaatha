1fc622c70f932988f624cb48664d60609300dcdf
Paul R Staab <staab@bio.lmu.de>
2014-08-21 13:50:26 +0200

    complete tree parsing script

--- src/parse_trees.cpp
+++ src/parse_trees.cpp
@@ -19,11 +19,7 @@ std::string parseTrees(std::string in_file, std::string out_file,
     std::string line;
     bool trio = false;
     
-    if (trio_opts.size() == 5) {
-      Rprintf("%f %f %f %f %f\n", trio_opts[0], trio_opts[1], 
-              trio_opts[2], trio_opts[3], trio_opts[4]);
-      trio = true;
-    }
+    if (trio_opts.size() == 5) trio = true;
     
     // Filter trees
     if (!trio) {
@@ -33,24 +29,52 @@ std::string parseTrees(std::string in_file, std::string out_file,
       }
     } else {
       // When using loci-trios, we need to remove the inter-locus regions.
-      size_t pos = 0, locus = 0, len = 0, locus_end = trio_opts[0];
+      size_t digits = 0, pos = 0, locus = 0, len = 0, seg_len = 0, 
+             locus_end = trio_opts[0];
+             
       while (getline(input, line)) {
         if (line.substr(0, 1) != "[") continue;
-        len = std::atoi(line.substr(1, line.find("]")-1).c_str());
+        digits = line.find("]")-1;
+        len = std::atoi(line.substr(1, digits).c_str());
         
-        if (pos + len == locus_end) {
-          // Everything is easy when the tree partition matches the locus' ends
-          pos += len;
+        while (pos + len >= locus_end) {
+          // Print tree until end of locus if neccessary
+          seg_len = locus_end - pos;
+          //Rprintf("locus %i (end %i) - pos %i - len %i\n", locus, locus_end, 
+          //        pos, len);
+                  
+          if (locus % 2 == 0) {
+            output << "[" << seg_len << "]" 
+                   << line.substr(digits+2, std::string::npos) << "\n";
+          }
+          
+          // Move to the end of the locus
+          len -= seg_len;
+          pos += seg_len;
           ++locus;
           locus_end += trio_opts[locus];
-        } else if (pos + len > locus_end) {
-          // First move to the end of the current locus / inter-locus region
           
-        } else {
-          pos += len;
-          if (locus % 2 == 1) output << line << "\n";
+          // Reset counter if we reached end of last locus.
+          if (locus == 5 && len == 0) {
+            locus = 0;
+            locus_end = trio_opts[0];
+            pos = 0;
+            len = 0;
+          }
+        }
+        
+        // If length is left, this now falls completely inside the current 
+        // locus.
+        if (len > 0) {
+           pos += len;
+           if (locus % 2 == 0) {
+            output << "[" << len << "]" 
+                   << line.substr(digits+2, std::string::npos) << "\n";
+           }
         }
       }
+      
+      if (pos != 0) stop("Error parsing trees");
     }
     
     input.close();
--- tests/testthat/test-u-cpp-parse-trees.R
+++ tests/testthat/test-u-cpp-parse-trees.R
@@ -29,7 +29,6 @@ positions: 0.3718 0.8443
 ", file=sim_output);
   
   expect_equal(parseTrees(sim_output, tree_file, NA), tree_file)
-  
   expect_true(file.exists(tree_file))
   trees <- scan(tree_file, "character", quiet=TRUE)
   expect_equal(length(trees), 6)
@@ -39,7 +38,36 @@ positions: 0.3718 0.8443
   expect_equal(trees[4], '[11](2:1.261,(1:0.015,3:0.015):1.246);')
   expect_equal(trees[5], '[2](3:0.613,(1:0.076,2:0.076):0.537);')
   expect_equal(trees[6], '[18](3:0.460,(1:0.076,2:0.076):0.384);')
+  unlink(tree_file)
   
-  parseTrees(sim_output, tree_file, c(2, 1, 3, 1, 3))
-  #expect_equal(, tree_file)
+  parseTrees(sim_output, tree_file, c(2, 4, 8, 2, 4))
+  expect_true(file.exists(tree_file))
+  trees <- scan(tree_file, "character", quiet=TRUE)
+  expect_equal(length(trees), 7)
+  expect_equal(trees[1], '[2](2:0.865,(1:0.015,3:0.015):0.850);')
+  expect_equal(trees[2], '[3](2:1.261,(1:0.015,3:0.015):1.246);')
+  expect_equal(trees[3], '[5](2:1.261,(1:0.015,3:0.015):1.246);')
+  expect_equal(trees[4], '[4](2:1.261,(1:0.015,3:0.015):1.246);')
+  
+  expect_equal(trees[5], '[2](3:0.613,(1:0.076,2:0.076):0.537);')
+  expect_equal(trees[6], '[8](3:0.460,(1:0.076,2:0.076):0.384);')
+  expect_equal(trees[7], '[4](3:0.460,(1:0.076,2:0.076):0.384);')
+  
+  parseTrees(sim_output, tree_file, c(9, 2, 2, 5, 2))
+  expect_true(file.exists(tree_file))
+  trees <- scan(tree_file, "character", quiet=TRUE)
+  expect_equal(length(trees), 9)
+  expect_equal(trees[1], '[2](2:0.865,(1:0.015,3:0.015):0.850);')
+  expect_equal(trees[2], '[3](2:0.865,(1:0.015,3:0.015):0.850);')
+  expect_equal(trees[3], '[4](2:1.261,(1:0.015,3:0.015):1.246);')
+  expect_equal(trees[4], '[2](2:1.261,(1:0.015,3:0.015):1.246);')
+  expect_equal(trees[5], '[2](2:1.261,(1:0.015,3:0.015):1.246);')
+  
+  expect_equal(trees[6], '[2](3:0.613,(1:0.076,2:0.076):0.537);')
+  expect_equal(trees[7], '[7](3:0.460,(1:0.076,2:0.076):0.384);')  
+  expect_equal(trees[8], '[2](3:0.460,(1:0.076,2:0.076):0.384);')
+  expect_equal(trees[9], '[2](3:0.460,(1:0.076,2:0.076):0.384);')
+  
+  expect_error(parseTrees(sim_output, tree_file, c(9, 2, 2, 5, 1)))
+  expect_error(parseTrees(sim_output, tree_file, c(19, 2, 2, 5, 1)))
 })
