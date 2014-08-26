9759916e1a3fd5e2454899768a5de024ff04f3aa
Paul R Staab <staab@bio.lmu.de>
2014-08-26 17:58:43 +0200

    update travis config

--- tests/integration/run-tests.R
+++ tests/integration/run-tests.R
@@ -1,7 +1,12 @@
 #!/usr/bin/Rscript
 library(testthat)
-library(jaatha)
+library(devtools)
+library(methods)
 
+# Install jaatha & attach its namespace
 if (file.exists("DESCRIPTION")) setwd("tests//integration")
+install.packages('../..', repos=NULL)
+attach(loadNamespace('jaatha'), name=paste("namespace", 'jaatha', sep=":"), pos=3)
+
 source("..//testthat//test-aaa-setup.R")
 test_dir('.', reporter='stop')
