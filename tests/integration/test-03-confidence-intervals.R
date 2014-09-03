2b5198cbd30f84e8b317d7a5c88da6db070ed58f
Paul R Staab <staab@bio.lmu.de>
2014-09-03 11:29:38 +0200

    add subset feature to confidence intervals

--- tests/integration/test-03-confidence-intervals.R
+++ tests/integration/test-03-confidence-intervals.R
@@ -1,11 +1,17 @@
 context("Confidence Intervals")
 
 test_that("Calculation of Confidence Intervals", {
-  jaatha <- Jaatha.initialSearch(jaatha.csi, 20, 1)
-  jaatha <- Jaatha.refinedSearch(jaatha, 1, 20, max.steps=10)
-  jaatha <- Jaatha.confidenceIntervals(jaatha, 0.95, 10, 1)
+  jaatha <- Jaatha.initialSearch(jaatha.csi, 10, 1)
+  jaatha <- Jaatha.refinedSearch(jaatha, 1, 10, max.steps=5)
+  jaatha <- Jaatha.confidenceIntervals(jaatha, 0.95, 20, 2)
   estimates <- t(Jaatha.getLikelihoods(jaatha)[1, -(1:2), drop = FALSE])
   conf.ints <- jaatha@conf.ints
   expect_equal(dim(conf.ints), c(2, 2))
   expect_true(all(conf.ints[, 1] <= estimates & estimates <= conf.ints[, 2]))
+  
+  logs <- tempfile('j-test-logs')
+  Jaatha.confidenceIntervals(jaatha, 0.95, 20, 2, logs, 1:10)
+  Jaatha.confidenceIntervals(jaatha, 0.95, 20, 2, logs, 11:20)
+  jaatha <- Jaatha.getCIsFromLogs(jaatha, 0.95, logs)
+  expect_equal(jaatha@conf.ints, conf.ints)
 })
