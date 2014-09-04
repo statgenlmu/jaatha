29c050c037fe2e9a97d6b7b1ae2ef392023832d8
Paul R Staab <staab@bio.lmu.de>
2014-09-04 13:53:11 +0200

    fix a unmapped memory access

--- src/parse_trees.cpp
+++ src/parse_trees.cpp
@@ -11,9 +11,12 @@ using namespace Rcpp;
 // [[Rcpp::export]]
 std::string parseTrees(std::string in_file, std::string out_file,
                        NumericVector trio_opts) {
-    // Open both files
-    std::ifstream input(in_file.c_str(), std::ifstream::in);
-    std::ofstream output(out_file.c_str(), std::ofstream::out);
+                         
+  // Open both files                      
+  std::ifstream input(in_file.c_str(), std::ifstream::in);
+  std::ofstream output(out_file.c_str(), std::ofstream::out);            
+  
+  try {
     if (!(input.is_open() && output.is_open())) stop("Failed to open file.");
     
     std::string line;
@@ -29,16 +32,24 @@ std::string parseTrees(std::string in_file, std::string out_file,
       }
     } else {
       // When using loci-trios, we need to remove the inter-locus regions.
-      size_t digits = 0, pos = 0, locus = 0, len = 0, seg_len = 0, 
-             locus_end = trio_opts[0];
+      size_t digits = 0,  // Number of digits of the length of the tree
+             pos = 0,     // Current position on the locus trio
+             locus = 0,   // The locus that we are currently in
+             len = 0,     // The length of the current tree
+             seg_len = 0, 
+             locus_end = trio_opts[0]; // The end of the current locus
              
       while (getline(input, line)) {
+        // Read in the next tree
         if (line.substr(0, 1) != "[") continue;
         digits = line.find("]")-1;
         len = std::atoi(line.substr(1, digits).c_str());
         
+        // If the current tree is valid for a sequence that ends behind the 
+        // end of the locus, we need to do a few things:
         while (pos + len >= locus_end) {
-          // Print tree until end of locus if neccessary
+          // First print a tree spanning until the end of the current locus 
+          // if neccessary.
           seg_len = locus_end - pos;
           //Rprintf("locus %i (end %i) - pos %i - len %i\n", locus, locus_end, 
           //        pos, len);
@@ -48,23 +59,26 @@ std::string parseTrees(std::string in_file, std::string out_file,
                    << line.substr(digits+2, std::string::npos) << "\n";
           }
           
-          // Move to the end of the locus
+          // Now the position move towards the end of the current locus.
           len -= seg_len;
           pos += seg_len;
-          ++locus;
-          locus_end += trio_opts[locus];
           
-          // Reset counter if we reached end of last locus.
-          if (locus == 5 && len == 0) {
+          // And look at the next locus.
+          if (locus < 4) {
+            ++locus;
+            locus_end += trio_opts[locus];
+          } else {
+            // The last tree should end exactly end at the end of the last locus
+            if (len != 0) stop("Tree and locus length do not match.");
+            // Reset the stats and go one to the next locus trio.
             locus = 0;
             locus_end = trio_opts[0];
             pos = 0;
-            len = 0;
           }
         }
         
-        // If length is left, this now falls completely inside the current 
-        // locus.
+        // If we are here the tree should end within the current locus.
+        // Print the rest and move until to the end of the tree.
         if (len > 0) {
            pos += len;
            if (locus % 2 == 0) {
@@ -80,4 +94,14 @@ std::string parseTrees(std::string in_file, std::string out_file,
     input.close();
     output.close();
     return(out_file);
+    
+  } catch( std::exception &ex ) {
+    forward_exception_to_r( ex );
+    input.close();
+    output.close();
+  } catch(...) { 
+    ::Rf_error( "c++ exception (unknown reason)" ); 
+    input.close();
+    output.close();
+  }
 }
