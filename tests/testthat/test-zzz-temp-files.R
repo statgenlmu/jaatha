110e072f5874b7ae0807e2c2352331e509afcad2
Paul R Staab <staab@bio.lmu.de>
2014-11-19 17:02:31 +0100

    improve style

--- tests/testthat/test-zzz-temp-files.R
+++ tests/testthat/test-zzz-temp-files.R
@@ -3,7 +3,7 @@ context("tmp file deletion")
 test_that('all temporary files are deleted', {
   temp_files <- list.files(tempdir(), pattern = '^jaatha_[0-9]+_')
   for (temp_file in temp_files) {
-    warning("Temp file not deleted: ", temp_file)
+    warning("temp file not deleted: ", temp_file)
   }
   expect_equal(length(temp_files), 0)
 })
