5e3c224ae4a66f0cdb69435d8cf2c9c3094ea331
Paul R Staab <staab@bio.lmu.de>
2015-01-14 14:16:43 +0100

    fix integration tests

--- tests/integration/test-02-smoothing-sumstats.R
+++ tests/integration/test-02-smoothing-sumstats.R
@@ -1,16 +1,10 @@
 context("Smoothing SumStats")
 
 test_that("test.smoothing", {
-  smooth.jaatha <- Jaatha.initialSearch(smooth.jaatha, 25, 2)
-  pStartPoints <- Jaatha.getStartingPoints(smooth.jaatha)
+  smooth_jaatha <- Jaatha.initialSearch(smooth_jaatha, 25, 2)
+  pStartPoints <- Jaatha.getStartingPoints(smooth_jaatha)
   expect_equal(nrow(pStartPoints), 4)
-  smooth.jaatha <- Jaatha.refinedSearch(smooth.jaatha, 1, 25, 25, max.steps = 9)
-  expect_true(ncol(smooth.jaatha@likelihood.table) == 4)
-  expect_true(nrow(smooth.jaatha@likelihood.table) >= 5)
-})
-
-test_that("test.smoothing.border", {
-  smooth.jaatha <- Jaatha.initialSearch(smooth.border.jaatha, 25, 2)
-  pStartPoints <- Jaatha.getStartingPoints(smooth.jaatha)
-  expect_equal(nrow(pStartPoints), 4)
+  smooth_jaatha <- Jaatha.refinedSearch(smooth_jaatha, 1, 25, 25, max.steps = 9)
+  expect_true(ncol(smooth_jaatha@likelihood.table) == 4)
+  expect_true(nrow(smooth_jaatha@likelihood.table) >= 5)
 })
--- tests/testthat/setup-custom-simulation-interface.R
+++ tests/testthat/setup-custom-simulation-interface.R
@@ -3,9 +3,9 @@ block.test <- new("Block")
 block.test@border <- matrix(c(0.4, 0.4, 0.6, 0.6), 2, 2) 
 
 csi.sim.func <- function(x, jaatha) {
-  list(data=c(rpois(3, x[1]), rpois(3, x[2])))
+  list(data=c(rpois(5, x[1]), rpois(5, x[2])))
 }
-csi.obs <- csi.sim.func(c(2:3))
+csi.obs <- csi.sim.func(c(3,5))
 csi.sum.stat <- R6::R6Class("Stat_PoiInd", inherit = jaatha:::Stat_Base)$new(csi.obs)
 csi.par.ranges <- matrix(c(0.1, 0.1, 10, 10), 2, 2)
 rownames(csi.par.ranges) <- c('x', 'y')
