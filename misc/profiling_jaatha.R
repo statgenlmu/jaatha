54c3d3523cabc099498d5fe4b966750450dc1173
Paul R Staab <staab@bio.lmu.de>
2015-01-21 15:43:29 +0100

    increase performance of fpc calculation

--- misc/profiling_jaatha.R
+++ misc/profiling_jaatha.R
@@ -11,18 +11,18 @@
 
 library(jaatha)
 
-dm <- dm.createThetaTauModel(c(20,25), 100)
-jsfs <- dm.simSumStats(dm.addSummaryStatistic(dm, 'seg.sites'), c(1,5))
+dm <- dm.createThetaTauModel(c(20,25), 20000)
+sim_data <- dm.simSumStats(dm.addSummaryStatistic(dm, 'seg.sites'), c(.1,5))
 jaatha <- Jaatha.initialize(jsfs, dm, use_fpc=TRUE, smoothing=FALSE)
 
-profile.is <- "./jaatha_is.prof"
+profile.is <- "./jaatha_is2.prof"
 gc()
 Rprof(profile.is, memory.profiling=TRUE)
 jaatha <- Jaatha.initialSearch(jaatha)
 Rprof(NULL)
 cat("Now run: R CMD Rprof", profile.is, "| less\n")
 
-profile.rs <- "./jaatha_rs"
+profile.rs <- "./jaatha_rs2.prof"
 gc()
 Rprof(profile.rs, memory.profiling=TRUE)
 jaatha <- Jaatha.refinedSearch(jaatha, 2)
--- src/sum_stat_fpc.cpp
+++ src/sum_stat_fpc.cpp
@@ -3,11 +3,12 @@
 
 using namespace Rcpp;
 
-int getType(size_t snp_1, size_t snp_2, 
-            const NumericVector positions, const NumericVector trio_locus) {
+inline int getType(size_t snp_1, size_t snp_2, 
+                   const NumericVector positions, 
+                   const NumericVector trio_locus) {
               
-  if (trio_locus(snp_1) == 0 && trio_locus(snp_2) == 0) {
-    return(std::abs(positions(snp_1) - positions(snp_2)) > 0.1); 
+  if (trio_locus[snp_1] == 0 && trio_locus[snp_2] == 0) {
+    return(std::abs(positions[snp_1] - positions[snp_2]) > 0.1); 
   }
   
   if (trio_locus(snp_1) != trio_locus(snp_2)) {
@@ -17,13 +18,14 @@ int getType(size_t snp_1, size_t snp_2,
   return 2;
 }
 
-bool is_singleton(const NumericMatrix seg_sites,
-                  const IntegerVector individuals,
-                  const size_t snp) {
+inline bool is_singleton(const NumericMatrix seg_sites,
+                         const IntegerVector individuals,
+                         const size_t n_ind,
+                         const size_t snp) {
                     
   size_t mut_count = 0;
-  for (int i = 0; i < individuals.size(); ++i) {
-    mut_count += seg_sites(individuals(i)-1, snp);
+  for (int i = 0; i < n_ind; ++i) {
+    mut_count += seg_sites(individuals[i]-1, snp);
   }
   
   return(mut_count == 1);
@@ -34,9 +36,10 @@ bool is_singleton(const NumericMatrix seg_sites,
 NumericMatrix calcPercentFpcViolation(const List seg_sites_list,
                                       const IntegerVector individuals,
                                       const NumericMatrix locus_length) {
-                                
+  
   size_t loci_number = seg_sites_list.size();
   if (loci_number != (size_t)locus_length.nrow()) stop("Locus number mismatch");
+  size_t n_ind = individuals.size();
   size_t type; // 0 = near, 1 = far, 2 = on outer locus, 3 = between loci;
   
   NumericMatrix violations(loci_number, 6);
@@ -45,7 +48,7 @@ NumericMatrix calcPercentFpcViolation(const List seg_sites_list,
         "mid_near", "mid_far", "outer", "between", "mid", "perc_polym")
     ); 
   
-  NumericVector total_count(4);
+  std::vector<size_t> total_count(4);
   std::vector<bool> combinations(4);
   NumericMatrix seg_sites;
   NumericVector positions, trio_locus;
@@ -62,23 +65,25 @@ NumericMatrix calcPercentFpcViolation(const List seg_sites_list,
     // Look at all pairs of SNPs
     for (int i = 0; i < seg_sites.ncol(); ++i) {
       // Ignore singletons
-      if (is_singleton(seg_sites, individuals, i)) continue;
+      if (is_singleton(seg_sites, individuals, n_ind, i)) continue;
       
       for (int j = i + 1; j < seg_sites.ncol(); ++j) {
-        // Ignore singletons
-        if (is_singleton(seg_sites, individuals, j)) continue;
         // Ignore SNP pairs between outer loci
-        if (std::abs(trio_locus(i) - trio_locus(j)) == 2) continue;
+        if (std::abs(trio_locus[i] - trio_locus[j]) == 2) continue;
+        
+        // Ignore singletons
+        if (is_singleton(seg_sites, individuals, n_ind, j)) continue;
+
+        // Get the type of the SNP pair
+        type = getType(i, j, positions, trio_locus);
         
         // Reset combination counter
         std::fill(combinations.begin(), combinations.end(), false);
-      
-        // Get the type of the SNP pair
-        type = getType(i, j, positions, trio_locus);
 
         // Count combinations
-        for (int k = 0; k < individuals.size(); ++k) {
-          combinations[2*seg_sites(individuals(k)-1, i) + seg_sites(individuals(k)-1, j)] = true;
+        for (int k = 0; k < n_ind; ++k) {
+          combinations[2*seg_sites(individuals[k]-1, i) + 
+                         seg_sites(individuals[k]-1, j)] = true;
         }
       
         // If we have all combinations
@@ -86,17 +91,17 @@ NumericMatrix calcPercentFpcViolation(const List seg_sites_list,
           ++violations(locus, type);
         }
 
-        ++total_count(type);
+        ++total_count[type];
       }
     }
 
     // Calculate %violations for all SNP pairs in middle locus
     violations(locus, 4) = (violations(locus, 0) + violations(locus, 1)) / 
-                           (total_count(0) + total_count(1));
+                           (total_count[0] + total_count[1]);
     
     // Calculate %violations for near and far SNP pairs in middle locus,
     // and for pairs between loci.
-    for (int i = 0; i < 4; ++i) violations(locus, i) /= total_count(i);
+    for (int i = 0; i < 4; ++i) violations(locus, i) /= total_count[i];
     
     // Calculate SNPs per basepair for middle locus
     violations(locus, 5) = sum(trio_locus == 0) / locus_length(locus, 2);
