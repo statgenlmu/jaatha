0e5eb081508f947118cbac5009e41594499eef15
Paul Staab <develop@paulstaab.de>
2015-04-30 15:14:21 +0200

    update model

--- R/jaatha_model.R
+++ R/jaatha_model.R
@@ -2,18 +2,24 @@
 jaatha_model_class <- R6Class("jaatha_model", lock = FALSE, 
   private = list(
     par_ranges = NA,
-    sum_stats = list()
+    sum_stats = list(),
+    add_statistic = function(stat) {
+      private$sum_stats[[stat$get_name()]] <- stat
+    }
   ),
   public = list(
     initialize = function(sim_func, par_ranges, sum_stats) {
+      assert_that(is.function(sim_func))
       private$sim_func = sim_func
-      private$par_ranges = par_ranges
+      private$par_ranges = par_range_class$new(par_ranges)
+      assert_that(is.list(sum_stats))
+      lapply(sum_stats, private$add_statistic)
       private$sum_stats = sum_stats
     },
     simulate = function(pars, seed) {
       # Simulate
       set.seed(seed)
-      sim_pars <- self$denormalize_pars(pars)
+      sim_pars <- private$par_ranges$denormalize(pars)
       sim_result <- private$sim_func(sim_pars)
       
       # Calculate Summary Statistics
@@ -26,17 +32,30 @@ jaatha_model_class <- R6Class("jaatha_model", lock = FALSE,
       sim_stats$pars_normal <- pars
       
       sim_stats
-    }
+    },
+    get_par_range = function() private$par_ranges
   )
 )
 
 
 create_jaatha_model <- function(sim_func, par_ranges, sum_stats, test = TRUE) {
-  model <- jaatha_model_class$new(sim_func, par_ranges, sum_stats)
-  
+  jaatha_model_class$new(sim_func, par_ranges, sum_stats)
 }
 
 
+create_test_model <- function() {
+  sim_func <- function(x, jaatha) rpois(20, x)
+  csi.obs <- csi.sim.func(c(3,5))
+  sum_stat <- R6::R6Class("Stat_PoiInd", inherit = jaatha:::Stat_Base, 
+                              private = list(mask=rep(c(TRUE,FALSE), 10)),
+                              public = list(transform = function(data) {
+                                c(sum(data[private$mask]), sum(data[!private$mask]))
+                              })
+  )$new(csi.obs, "csi")
+  par_ranges <- matrix(c(0.1, 0.1, 10, 10), 2, 2)
+  rownames(par_ranges) <- c("x", "y")
+  create_jaatha_model(sim_func, par_ranges, list(sum_stat))
+}
 
 
 
--- tests/testthat/test-jaatha-model.R
+++ tests/testthat/test-jaatha-model.R
@@ -8,3 +8,11 @@ test_that("jaatha model can be initialized", {
                       matrix(c(1, 2), 1, 2),
                       list(stat))
 })
+
+test_that("simulation works", {
+  model <- create_test_model()
+  res <- model$simulate(pars = c(1, 1), seed = 1)
+  expect_that(res, is_a("list"))
+  expect_equivalent(res$pars, c(10, 10))
+  expect_equivalent(res$pars_normal, c(1, 1))
+})
