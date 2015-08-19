0647c28fd6ad3ef61c4c074f291b520c56c205af
Paul Staab <develop@paulstaab.de>
2015-08-19 13:27:37 +0200

    remove unused function

--- R/tools.R
+++ R/tools.R
@@ -3,9 +3,5 @@ is_single_numeric <- function(value) is.numeric(value) && length(value) == 1
 is_single_logical <- function(value) is.logical(value) && length(value) == 1
 
 
-# --- Printing -----------------------------------------------------------------
-.print <- function(...) { cat(..., "\n") }
-
-
 # --- Seeds --------------------------------------------------------------------
 sample_seed <- function(n = 1) sample.int(2 ^ 20, n)
