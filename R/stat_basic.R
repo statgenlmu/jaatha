9f5927a95dfa8fa2e2cdc73935eda6326134324d
Paul Staab <develop@paulstaab.de>
2015-08-28 11:08:05 +0200

    add documentation for create_jaatha_stat

--- R/stat_basic.R
+++ R/stat_basic.R
@@ -18,6 +18,44 @@ stat_basic_class <- R6Class("jaatha_stat_basic",
 )
 
 
+#' Create a summary statistic for Jaatha
+#' 
+#' This function creates summary statistics for Jaatha models. A summary
+#' statistic consists primarily of a function that calculates the statistic
+#' from the simulation results. Jaatha primarily supports Poisson distributed
+#' summary statistics, but can also transform summary statistics that follow
+#' a different distribution in approximately Poisson distributed statistics.
+#' 
+#' @section Transformation of non Poisson distributed statistics:
+#'   To transform a statistic into approximately Poisson distributed values,
+#'   we first calculate the empirical quantiles of the real data for the 
+#'   probabilities given in \code{breaks}. These are used as break points for
+#'   divining the range of the statistic into disjunct intervals. We then count
+#'   who many of the values for the simulated data fall into each intervals, and
+#'   use this counts as summary statistic. The counts are multinomial 
+#'   distributed, and should be close to the required Poisson distribution in 
+#'   most cases.
+#'     
+#' @param name The name of the summary statistic
+#' @param calc_func The function that summarizes the simulation data. Must take
+#'   two arguments. The first is the simulated data, and the second are
+#'   options that can be calculated from the real data. Ignoring the second 
+#'   argument in the function body should be fine in most situations. The 
+#'   function must return a numeric vector if \code{poisson = TRUE}, and can
+#'   also return a numeric matrix if \code{poisson = FALSE}.
+#' @param poisson If \code{TRUE}, it is assumed that the summary statistic
+#'   values are (at least approximately) independent and Poisson distributed.
+#'   If it is set to \code{FALSE}, the statistic is transformed into an approximately
+#'   Poisson distributed array using a binning approach. See "Transformation
+#'   of non Poisson distributed statistics" for details. If any summary
+#'   statistic is only approximately Poisson distributed, Jaatha is a
+#'   composite-likelihood method.
+#' @param breaks The probabilities for the quantiles that are used for binning
+#'   the data. See the section on non Poisson distributed summary statistics
+#'   for details.
+#' @return The summary statistic. Indented for being used with
+#'   \code{\link{create_jaatha_model}}.
+#' @export
 create_jaatha_stat <- function(name, calc_func, poisson = TRUE, 
                                breaks = c(.1, .5, .9)) {
   
