80c11437405527cc834c60fd2dc3ee02a5930e03
Paul Staab <develop@paulstaab.de>
2015-09-03 14:09:30 +0200

    update profile script

--- misc/profile_jaatha.R
+++ misc/profile_jaatha.R
@@ -1,19 +1,22 @@
 # Example script for profiling the initiual and refined search.
 
-dm <- dm.createThetaTauModel(c(20,25), 200)
-sim_data <- dm.simSumStats(dm.addSummaryStatistic(dm, 'seg.sites'), c(.1,5))
-jaatha <- Jaatha.initialize(sim_data, dm, use_fpc=TRUE, smoothing=FALSE)
+model <- create_test_model()
+data <- create_test_data(model)
 
-profile.is <- tempfile('jaatha_is2.prof')
-gc()
-Rprof(profile.is, memory.profiling=TRUE)
-jaatha <- Jaatha.initialSearch(jaatha)
-Rprof(NULL)
-summaryRprof(profile.is, memory = 'both')
+proffile <- tempfile('jaatha_is2.prof')
 
-profile.rs <- "./jaatha_rs2.prof"
 gc()
-Rprof(profile.rs, memory.profiling=TRUE)
-jaatha <- Jaatha.refinedSearch(jaatha, 2)
+set.seed(17)
+
+Rprof(proffile, memory.profiling = TRUE)
+results <- jaatha(model, data, repetitions = 2, sim = 500, cores = 2)
 Rprof(NULL)
-summaryRprof(profile.rs, memory = 'both')
+
+summaryRprof(filename = proffile, memory = 'both')
+
+
+
+# devtools::install_github("hadley/lineprof")
+library(lineprof)
+pr <- lineprof(results <- jaatha(model, data, repetitions = 2, sim = 100, cores = 1))
+shine(pr)
