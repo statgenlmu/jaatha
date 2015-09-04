70c451ebbb7c66e89c950c856506d525bb002fdb
Paul Staab <develop@paulstaab.de>
2015-09-04 13:23:55 +0200

    add function to check for suggested packages

--- R/tools.R
+++ R/tools.R
@@ -5,3 +5,12 @@ is_single_logical <- function(value) is.logical(value) && length(value) == 1
 
 # --- Seeds --------------------------------------------------------------------
 sample_seed <- function(n = 1) sample.int(2 ^ 20, n)
+
+
+# --- Check for suggested packages
+require_package <- function(pkg) {
+  if (!requireNamespace(pkg, quietly = TRUE)) {
+    stop("Please install package '", pkg, "'", call. = FALSE)
+  }
+  invisible(TRUE)
+} 
