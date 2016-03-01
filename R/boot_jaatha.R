e53fa595b1c0129cf1074447f7799ece3eabfed0
Paul Staab <develop@paulstaab.de>
2016-03-01 17:04:34 +0100

    add `cores_per_run` option

--- R/boot_jaatha.R
+++ R/boot_jaatha.R
@@ -4,29 +4,53 @@
 #' function to bootstrap Jaatha estimates. Each bootstap replication requires
 #' a complete jaatha estimation on data simulated with the original parameter
 #' estimates. Therefore, bootstrapping is normally computationally demanding and
-#' should be executed on a computing cluster. The jaatha analyses are
-#' resticted to a single CPU cores, so that as many replicas as possible can 
-#' be executed in parallel using the corresponding options of 
-#' \code{\link[boot]{boot}}.
+#' should be executed on a computing cluster.
 #' 
 #' @param results The results of an \code{\link{jaatha}} analysis.
 #' @param R The number of bootstrapping replicates that are performed.
+#' @param cores_per_run The number of cores that are used for each replicate.
+#'        This corresponds to the \code{cores} option of \code{\link{jaatha}}.
+#'        Different replicates can be executed in parallel using the 
+#'        \code{parallel}, \code{ncpus} and \code{cl} options of 
+#'        \code{\link[boot]{boot}}.  The total number of CPU cores
+#'        used is \code{ncpus} * \code{cores_per_run}.
 #' @param ... Additional arguments that are passed on \code{\link[boot]{boot}}.
 #'   It is highly recommended to use its \code{parallel} and \code{ncpus} 
 #'   options to parallelize the bootstrap replicates.
 #' @return The result of \code{\link[boot]{boot}}. This object can be used to
 #'   estimate standard errors or confidence intervals of the estimates using
 #'   the functions available in package \pkg{boot}.
+#' @seealso 
+#' \code{\link[boot]{boot}}, \code{\link{jaatha}}
+#' 
+#' @examples 
+#' \dontrun{
+#' # The original Jaatha anaylsis:
+#' jaatha_result <- jaatha(model, data, cores = 4)
+#' 
+#' # Bootstrapping the results using 4 CPU cores on host1 and 2 on host2/host3:
+#' library(boot)
+#' library(snow)
+#' cl <- makeSOCKcluster(c("host1", "host1", "host2", "host3"))
+#' 
+#' jaatha_boot_results <- boot_jaatha(jaatha_result, 200, 
+#'                                    cores_per_run = 2,
+#'                                    parallel = "snow",
+#'                                    cl = cl)
+#' 
+#' stopCluster(cl)
+#' boot.ci(jaatha_boot_results, type = "norm")
+#' }
 #' 
 #' @export
-boot_jaatha <- function(results, R, ...) {
+boot_jaatha <- function(results, R, cores_per_run = 1, ...) {
   require_package("boot")
   if (R.Version()$major == 3 && R.Version()$minor < 2.2) {
     stop("This function requires at least R Version 3.2.2")
   }
   
   args <- results$args
-  args$cores <- 1
+  args$cores <- cores_per_run
   model <- args$model
   sim_func <- model$get_sim_func()
   
@@ -50,7 +74,7 @@ boot_jaatha <- function(results, R, ...) {
   }
   
   boot::boot(0, jaatha_stat, R = R, 
-             sim = "parametric", 
+             sim = "parametric",
              ran.gen = simulate_data, 
              mle = results$estimate, ...)
 }
